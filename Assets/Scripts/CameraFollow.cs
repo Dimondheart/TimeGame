d1651445da54aba6d242df1b14e5713579373148
Bryan Bettis <bryancbettis@gmail.com>
2017-08-09 16:21:52 -0700

    Implemented Player Movement
    -Added player
    -Added player movment
    -Added camera follow

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+	public GameObject follow;
+
+	private Vector3 offset;
+
+	private void Start()
+	{
+		if (follow != null)
+		{
+			offset = transform.position - follow.transform.position;
+		}
+	}
+
+	private void OnPreCull()
+	{
+		if (follow == null)
+		{
+			return;
+		}
+		transform.position = follow.transform.position + offset;
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	public float movementSpeed = 5.0f;
+
+	private void Update()
+	{
+		GetComponent<Rigidbody2D>().velocity =
+			new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0.0f).normalized
+			* movementSpeed;
+	}
+}
