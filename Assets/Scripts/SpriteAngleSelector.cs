7af61ca6f52aca89238d640e809ec20bad6a9de3
Bryan Bettis <bryancbettis@gmail.com>
2017-08-13 17:01:19 -0700

    Prototype Sprite Stuff
    -Added a script to select front/left/right/back sprite based on current
    velocity
    -Added Green Sime (prototype enemy) sprites
    -Adjusted enemy prefabs to work with the new sprite selector

---
+++ Assets/Scripts/SpriteAngleSelector.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteAngleSelector : MonoBehaviour
+{
+	public Sprite frontSprite;
+	public Sprite backSprite;
+	public Sprite leftSprite;
+	public Sprite rightSprite;
+
+	private void Update()
+	{
+		float verticalAngle = Vector2.Angle(GetComponent<Rigidbody2D>().velocity, Vector2.up);
+		if (verticalAngle <= 45.0f)
+		{
+			GetComponent<SpriteRenderer>().sprite = backSprite;
+		}
+		else if (verticalAngle >= 135.0f)
+		{
+			GetComponent<SpriteRenderer>().sprite = frontSprite;
+		}
+		else
+		{
+			float horizontalAngle = Vector2.Angle(GetComponent<Rigidbody2D>().velocity, Vector2.right);
+			if (horizontalAngle < 45.0f)
+			{
+				GetComponent<SpriteRenderer>().sprite = rightSprite;
+			}
+			else if (horizontalAngle > 135.0f)
+			{
+				GetComponent<SpriteRenderer>().sprite = leftSprite;
+			}
+		}
+	}
+}
